d710041ee9d19d79fb520c9a2d1d4663b00e013a
Muhammad Mehdi <96532589+edoitachi@users.noreply.github.com>
2023-05-01 11:26:20 +0500

    Create cards-data.js

---
+++ cards-data.js
@@ -0,0 +1,99 @@
+[
+  {
+    "id": 1,
+    "thumbnail": "https://picsum.photos/id/1/300/400",
+    "eventName": "Summer Music Festival",
+    "date": "14 August",
+    "location": "Beach"
+  },
+  {
+    "id": 2,
+    "thumbnail": "https://picsum.photos/id/2/300/400",
+    "eventName": "Art Exhibition",
+    "date": "28 June",
+    "location": "Gallery"
+  },
+  {
+    "id": 3,
+    "thumbnail": "https://picsum.photos/id/3/300/400",
+    "eventName": "Charity Auction",
+    "date": "10 September",
+    "location": "Ballroom"
+  },
+  {
+    "id": 4,
+    "thumbnail": "https://picsum.photos/id/4/300/400",
+    "eventName": "Street Food Festival",
+    "date": "3 July",
+    "location": "Park"
+  },
+  {
+    "id": 5,
+    "thumbnail": "https://picsum.photos/id/5/300/400",
+    "eventName": "Fashion Show",
+    "date": "22 March",
+    "location": "Theater"
+  },
+  {
+    "id": 6,
+    "thumbnail": "https://picsum.photos/id/6/300/400",
+    "eventName": "Movie Screening",
+    "date": "19 August",
+    "location": "Cinema"
+  },
+  {
+    "id": 7,
+    "thumbnail": "https://picsum.photos/id/7/300/400",
+    "eventName": "Wine Tasting",
+    "date": "7 September",
+    "location": "Vineyard"
+  },
+  {
+    "id": 8,
+    "thumbnail": "https://picsum.photos/id/8/300/400",
+    "eventName": "Book Fair",
+    "date": "15 April",
+    "location": "Library"
+  },
+  {
+    "id": 9,
+    "thumbnail": "https://picsum.photos/id/9/300/400",
+    "eventName": "Crafts Market",
+    "date": "2 June",
+    "location": "Marketplace"
+  },
+  {
+    "id": 10,
+    "thumbnail": "https://picsum.photos/id/10/300/400",
+    "eventName": "Dog Show",
+    "date": "11 July",
+    "location": "Park"
+  },
+  {
+    "id": 11,
+    "thumbnail": "https://picsum.photos/id/11/300/400",
+    "eventName": "Beer Festival",
+    "date": "8 October",
+    "location": "Brewery"
+  },
+  {
+    "id": 12,
+    "thumbnail": "https://picsum.photos/id/12/300/400",
+    "eventName": "Stand-Up Comedy Night",
+    "date": "5 May",
+    "location": "Club"
+  },
+  {
+    "id": 13,
+    "thumbnail": "https://picsum.photos/id/13/300/400",
+    "eventName": "Cooking Workshop",
+    "date": "21 September",
+    "location": "Kitchen"
+  },
+  {
+    "id": 14,
+    "thumbnail": "https://picsum.photos/id/14/300/400",
+    "eventName": "Dance Performance",
+    "date": "17 August",
+    "location": "Theater"
+  }]
